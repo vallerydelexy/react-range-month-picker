4aa243b84f3082ebdd96e179bdc626957b794985
Rizki Aprita <vallerydelexy@gmail.com>
2024-06-09 21:03:45 +0700

    month range picker

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export { MonthPicker } from './components/MonthPicker/MonthPicker.jsx';
+export { MonthRangePicker } from './components/MonthRangePicker/MonthPicker.jsx';
 export { MonthInput } from './components/MonthInput/MonthInput.jsx';
+
